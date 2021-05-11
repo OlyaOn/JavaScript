cadfe40ccffaa102166ccbe85466f8eaeb02290e
Olya <kuprienko.olya@gmail.com>
2021-05-11 18:25:21 +0300

    add new script.js

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 'use strict' /*пишем на стандарте us6*/
 
-var number  = 5;
-var string  = 'Hello!'
+let money = prompt("Ваш бюджет на месяц?"),
+           time = prompt("Введите дату в формате YYYY-MM-DD");
 
-let something;
-console.log(something);
 
-let persone = {
-    name: 'John',
-    age: 25,
-    isMarried: false
+let appData = {
+    budget: money,
+    timeData:time,
+    expenses: {},//объект с обязательными расходами
+    optionalExpenses: {}, //объект с необязательными расходами
+    income: [],//массив данных с доп. доходом
+    savings: false
 };
-console.log(persone.name);
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
 
-let arr = ['plum.png','img.jpeg','apple.png'];
-console.log(arr[0]);
-/*alert("hello,world!");*/
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
 
-/*let answer = confirm("Are you here?");
-
-console.log(answer);*/
-
-let answer = prompt("Есть ли Вам 18?", "");
-
-console.log(typeof(arr));
-
-let incr = 10,
-    decr = 10;
-
-console.log(incr++);
-console.log(decr-- );
+alert(appData.budget / 30);
---
+++ script_1.js
@@ -0,0 +1,32 @@
+'use strict' /*пишем на стандарте us6*/
+
+var number  = 5;
+var string  = 'Hello!'
+
+let something;
+console.log(something);
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+console.log(persone.name);
+
+let arr = ['plum.png','img.jpeg','apple.png'];
+console.log(arr[0]);
+/*alert("hello,world!");*/
+
+/*let answer = confirm("Are you here?");
+
+console.log(answer);*/
+
+let answer = prompt("Есть ли Вам 18?", "");
+
+console.log(typeof(arr));
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr-- );
